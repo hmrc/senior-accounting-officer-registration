93a18fd83bac4f113b7f484c34e0a90e155a203c
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2025-06-18 15:16:45 +0100

    SAOD-283 library bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,15 +8,15 @@ object AppDependencies {
   private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test
   )
 
-  val it = Seq.empty
+  val it: Seq[ModuleID] = Seq.empty
 }
